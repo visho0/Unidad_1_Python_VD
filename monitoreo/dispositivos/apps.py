edd0d0a08473f6a4a67cd22929e4a2d344e108c7
vicho <visho.duran.205@gmail.com>
2025-08-22 13:42:47 -0400

    actividad 1

--- monitoreo/dispositivos/apps.py
+++ monitoreo/dispositivos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DispositivoConfig(AppConfig):
+class DispositivosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dispositivo'
+    name = 'dispositivos'
---
+++ monitoreo/dispositivos/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+def inicio(request):
+    contexto = {"nombre": "Profe Javier"}
+    productos = [
+        {"nombre": "Sensor 1", "valor": 100},
+        {"nombre": "Sensor 2", "valor": 200},
+        {"nombre": "Sensor 3", "valor": 300},
+    ]
+
+    return render(request, "dispositivos/inicio.html", {
+        "contexto": contexto,
+        "productos": productos
+    })
+
+def panel_dispositivo(request):
+    dispositivos = [
+        {"nombre": "Sensor Temperatura", "consumo": 50},
+        {"nombre": "Medidor Solar", "consumo": 120},
+        {"nombre": "Sensor Movimiento", "consumo": 30},
+        {"nombre": "Calefactor", "consumo": 200},
+    ]
+
+    consumo_maximo = 100
+
+    criticos = sum(1 for d in dispositivos if d["consumo"] > consumo_maximo)
+
+    return render(request, "dispositivos/panel_dispositivo.html", {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo
+    })
