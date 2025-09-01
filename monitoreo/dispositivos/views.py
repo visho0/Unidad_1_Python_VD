c5b2fa60d56a1f50f524b864bfa694716bea13db
vicho <visho.duran.205@gmail.com>
2025-09-01 18:14:11 -0400

    clase 01-09

--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,34 +1,31 @@
 from django.shortcuts import render
+from .models import Dispositivo
 
 def inicio(request):
-    contexto = {"nombre": "Profe Javier"}
-    productos = [
-        {"nombre": "Sensor 1", "valor": 100},
-        {"nombre": "Sensor 2", "valor": 200},
-        {"nombre": "Sensor 3", "valor": 300},
-    ]
+    #Dispositivos = Dispositivo.objects.all()
+    Dispositivos = Dispositivo.objects.select_related("Categoria")
 
     return render(request, "dispositivos/inicio.html", {
-        "contexto": contexto,
-        "productos": productos
+        "dispositivos": Dispositivos
     })
 
 def panel_dispositivo(request):
-    dispositivos = [
-        {"nombre": "Sensor Temperatura", "consumo": 50},
-        {"nombre": "Medidor Solar", "consumo": 120},
-        {"nombre": "Sensor Movimiento", "consumo": 30},
-        {"nombre": "Calefactor", "consumo": 200},
-    ]
+   dispositivos = [
+       {"nombre": "Sensor Temperatura", "consumo": 50},
+       {"nombre": "Medidor Solar", "consumo": 120},
+       {"nombre": "Sensor Movimiento", "consumo": 30},
+       {"nombre": "Calefactor", "consumo": 200},
+   ]
+   consumo_maximo = 100
+   criticos = sum(1 for d in dispositivos if d["consumo"] > consumo_maximo)
+   contexto = {
+       "dispositivos": dispositivos,
+       "consumo_maximo": consumo_maximo,
+       "criticos": criticos
+   }
+   return render(request, "dispositivos/panel.html", contexto)
 
-    consumo_maximo = 100
-
-    criticos = sum(1 for d in dispositivos if d["consumo"] > consumo_maximo)
-
-    contexto = {
-        "dispositivos": dispositivos,
-        "consumo_maximo": consumo_maximo,
-        "criticos": criticos
-    }
-
-    return render(request, "dispositivos/panel.html", contexto)  
+def dispositivo(request, dispositivo_id):
+    dispositivo = Dispositivo.objects.get(id=dispositivo_id)
+    
+    return render(request, "dispositivos/inicio.html", {"dispositivo": dispositivo})
